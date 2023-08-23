b02ea1a4e9622e3b207ef666a9391b30348f787e
xulihang <xulihanghai@163.com>
2023-08-23 18:09:51 +0800

    install and init Dynamsoft Barcode Reader

--- Barcode Scanner/MainWindow.xaml.cs
+++ Barcode Scanner/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Dynamsoft;
+using Dynamsoft.DBR;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,14 +25,29 @@ namespace Barcode_Scanner
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        public BarcodeReader reader { get; set; }
         public MainWindow()
         {
             this.InitializeComponent();
+            this.InitBarcodeReader();
         }
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
             myButton.Content = "Clicked";
+            System.Diagnostics.Debug.WriteLine("Clicked");
+        }
+
+        private void InitBarcodeReader() {
+            string errorMsg;
+            EnumErrorCode errorCode = BarcodeReader.InitLicense("t0073oQAAACQUYF/x9rrKvXlLXTqx4ZcSmAZxrQ0Wdup4wSrA9cTE833mG4PB2q6PADieZ2scQd7NEVJ9LbY7AipZSK5QnFXf0H8iWQ==", out errorMsg);
+            if (errorCode != EnumErrorCode.DBR_SUCCESS)
+            {
+                // Add your code for license error processing;
+                System.Diagnostics.Debug.WriteLine(errorMsg);
+            }
+            reader = new BarcodeReader();
+            System.Diagnostics.Debug.WriteLine(reader.GetVersion());
         }
     }
 }
